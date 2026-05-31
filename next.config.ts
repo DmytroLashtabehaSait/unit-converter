a5af92e2f2c7b988787412ba68b9e218614c223c
Daniil Sakov <sakovdaniil62886271@gmail.com>
2026-05-30 18:12:52 -0600

    Configure Deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "export",
+	basePath: "/unit-converter",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
